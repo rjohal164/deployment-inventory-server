6c8abdd1514f366bb1e17aa8241e6ef9a2fec818
rjohal164 <r.johal164@gmail.com>
2025-09-30 19:09:49 -0700

    Fix API_URL assignment in productApi.js to correctly reference the environment variable VITE_API_URL

--- src/api/productApi.js
+++ src/api/productApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // import.meta.env.VITE_API_URL;
-const API_URL = "import.meta.env.VITE_API_URL";
+const API_URL = import.meta.env.VITE_API_URL;
 
 // Get all products
 export const getProducts = async () => {
